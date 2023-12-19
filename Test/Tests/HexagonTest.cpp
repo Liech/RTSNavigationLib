ebb96f4cef845be41a2ec6c2ae0ac5b2785123d7
Liech <nicolas_harttig@hotmail.de>
2023-12-20 00:03:33 +0100

    voxelizer test

--- Test/Tests/HexagonTest.cpp
+++ Test/Tests/HexagonTest.cpp
@@ -5,26 +5,35 @@
 #include <RTSPathingLib/Util/svg.h>
 #include <RTSPathingLib/Hexagon/Hex2DVoxelizer.h>
 
-TEST_CASE("Hexagon/wub", "[Hexagon]") {
-  glm::ivec2 dimension = glm::ivec2(20, 20);
+TEST_CASE("Hexagon/voxelizer", "[HexagonVoxelizer]") {
+  glm::ivec2 dimension = glm::ivec2(6, 6);
 
   std::vector<glm::vec2> polygon;
-  glm::vec2 offset = glm::vec2(4, 4);
-  float scale = 5;
-  polygon.push_back(offset + glm::vec2(0, 0) * scale);
+  glm::vec2 offset = glm::vec2(1, 1);
+  float scale = 0.6;
   polygon.push_back(offset + glm::vec2(1, 0) * scale);
-  polygon.push_back(offset + glm::vec2(1, 1) * scale);
-  polygon.push_back(offset + glm::vec2(0, 1) * scale);  
-  polygon.push_back(offset + glm::vec2(0, 0) * scale);
+  polygon.push_back(offset + glm::vec2(3, 0) * scale);
+  polygon.push_back(offset + glm::vec2(4.5, 6) * scale);
+  polygon.push_back(offset + glm::vec2(0, 4) * scale);  
+  polygon.push_back(offset + glm::vec2(1, 0) * scale);
   svg v;
   v.streak = polygon;
   v.color = "blue";
 
   auto t = Hex2DVoxelizer::voxelize(polygon, dimension, glm::vec2(0,0),1);
-  std::vector<svg> x = HexGridSvg::write(t, 1);
+  //std::vector<svg> x = HexGridSvg::write(t, 1);
+  //x.push_back(v);
+  //svg::write("Hello.svg", x, glm::vec2(-5, -5), glm::vec2(30, 30));
 
-  x.push_back(v);
-  svg::write("Hello.svg", x, glm::vec2(-5, -5), glm::vec2(30, 30));
+  std::vector<bool> r = {
+    false,false,false,false,false,false,
+    false,false,false,true ,false,false,
+    false,false,true ,true ,true ,false,
+    false,false,true ,true ,true ,false,
+    false,false,false,true ,true ,false,
+    false,false,false,false,false,false
+  };
 
-  REQUIRE(true);
+  REQUIRE(t.data.size() == r.size());
+  REQUIRE(t.data == r);
 }
