ae8896e98e02c9c6032549d914088a968f35c831
Liech <nicolas_harttig@hotmail.de>
2023-12-21 13:27:02 +0100

    formation calculator progress

--- RTSPathingLib/Source/RTSPathingLib/Formation/FormationCalculator.cpp
+++ RTSPathingLib/Source/RTSPathingLib/Formation/FormationCalculator.cpp
@@ -2,13 +2,65 @@
 
 #include "Formation.h"
 #include "Body.h"
+#include "Formation/FormationShape/FormationShape.h"
 
 namespace RTSPathingLib {
   std::vector<Body> FormationCalculator::calculate(const Formation& formation, const std::vector<Body>& units) {
     glm::mat4 currentTransformation = glm::mat4(1);
-    auto sizes = getSizesPerCategory(units);
+    auto overall = getSizesPerCategory(units);
+    auto current = overall;
+    auto maxWeights = getCategoryWeightSum(formation);
+
+
+    auto unitsPlacedHere = gatherUnits(formation, overall, current);
+    auto formationSize   = getSizeSum(unitsPlacedHere);
+    auto shape           = formation.getShape().getPolygon();
+
 
     return units;
+  }
+
+  size_t FormationCalculator::getSizeSum(const std::map<size_t, size_t>& units) {
+    size_t result = 0;
+    for (const auto& x : units) {
+      result += x.first * x.second;
+    }
+    return result;
+  }
+
+  std::map<size_t, size_t> FormationCalculator::gatherUnits(const Formation& formation, const std::map<size_t, std::map<size_t, size_t>>& OverallUnits, std::map<size_t, std::map<size_t, size_t>>& availableUnits) {
+    size_t category = formation.getUnitCategory();
+    float weight = formation.getUnitDistributionWeight();
+    std::map<size_t, size_t> unitAmount;
+    for (const auto& size : OverallUnits.at(category)) {
+      size_t amount = std::ceil(weight * size.second);
+      size_t available = availableUnits[category][size.first];
+      if (available > amount)
+        amount = available;
+      unitAmount[size.first] = amount;
+      availableUnits[category][size.first] -= amount;
+    }
+    return unitAmount;
+  }
+
+  std::map<size_t, float> FormationCalculator::getCategoryWeightSum(const Formation& formation) {
+    std::map<size_t, float> result;
+    size_t category = formation.getUnitCategory();
+    if (result.count(category) == 0)
+      result[category] = 0;
+    result[category] += formation.getUnitDistributionWeight();
+
+    for (size_t i = 0; i < formation.getChildrenCount(); i++) {
+      auto& child = formation.getChild(i);
+      auto sub = getCategoryWeightSum(child);
+      for (auto& keyvalue : sub) {
+        if (result.count(keyvalue.first) == 0)
+          result[keyvalue.first] = keyvalue.second;
+        else
+          result[keyvalue.first] += keyvalue.second;
+      }
+    }
+    return result;
   }
 
   std::map<size_t, std::map<size_t, size_t>> FormationCalculator::getSizesPerCategory(const std::vector<Body>& units) {
--- RTSPathingLib/Source/RTSPathingLib/Formation/FormationCalculator.h
+++ RTSPathingLib/Source/RTSPathingLib/Formation/FormationCalculator.h
@@ -8,12 +8,16 @@
 namespace RTSPathingLib {
   struct Body;
   class Formation;
+  class RectangleGrid;
 
   class FormationCalculator {
   public:
     static std::vector<Body> calculate(const Formation&, const std::vector<Body>& units);
 
   private:
+    static size_t getSizeSum(const std::map<size_t, size_t>&);
+    static std::map<size_t, size_t> gatherUnits(const Formation&, const std::map<size_t, std::map<size_t, size_t>>& OverallUnits, std::map<size_t, std::map<size_t, size_t>>& availableUnits);
     static std::map<size_t, std::map<size_t, size_t>> getSizesPerCategory(const std::vector<Body>& units);
+    static std::map<size_t, float> getCategoryWeightSum(const Formation&);
   };
 }
