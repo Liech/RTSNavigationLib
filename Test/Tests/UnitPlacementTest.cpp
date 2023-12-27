fd4a4bb441524770fb2c5540d37298e47b826fca
Nicolas Harttig <nicolas_harttig@hotmail.de>
2023-12-27 23:11:48 +0100

    mixed test

--- Test/Tests/UnitPlacementTest.cpp
+++ Test/Tests/UnitPlacementTest.cpp
@@ -119,4 +119,31 @@ namespace BodyUtilTests {
       result[1].position.x == result[0].position.x - 2)
     );
   }
+
+  TEST_CASE("UnitPlacement/Mixed", "[UnitPlacement]") {
+    RTSPathingLib::RectangleGrid<bool> grid;
+    grid.dimension = glm::ivec2(4, 2);
+    grid.data = { true , true , true , true ,
+                  true , true , true , true , };
+    grid.offset = glm::dvec2(82, 82);
+    std::map<size_t, size_t> unitsToPlace;
+    unitsToPlace[1] = 4;
+    unitsToPlace[2] = 1;
+    size_t category = 123;
+    RTSPathingLib::UnitPlacement placement(grid, unitsToPlace, 123);
+
+    bool success = false;
+    std::vector<RTSPathingLib::Body> result = placement.place(success);
+    REQUIRE(success);
+    REQUIRE(result.size() == 5);
+    REQUIRE(result[0].size == 2);
+    REQUIRE(result[1].size == 1);
+    REQUIRE(result[2].size == 1);
+    REQUIRE(result[3].size == 1);
+    REQUIRE(result[4].size == 1);
+    REQUIRE(glm::distance(result[0].position, result[1].position) >= 2);
+    REQUIRE(glm::distance(result[0].position, result[2].position) >= 2);
+    REQUIRE(glm::distance(result[0].position, result[3].position) >= 2);
+    REQUIRE(glm::distance(result[0].position, result[4].position) >= 2);
+  }
 }
