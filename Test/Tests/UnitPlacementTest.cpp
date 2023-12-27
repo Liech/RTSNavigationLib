4da172fdc6b69ff95c2c213174d0de800b11f164
Nicolas Harttig <nicolas_harttig@hotmail.de>
2023-12-27 21:26:50 +0100

    added should fail tests

--- Test/Tests/UnitPlacementTest.cpp
+++ Test/Tests/UnitPlacementTest.cpp
@@ -23,10 +23,28 @@ namespace BodyUtilTests {
     REQUIRE(result[0].category == category);
   }
 
-  TEST_CASE("UnitPlacement/OneSize2", "[UnitPlacement]") {
+  TEST_CASE("UnitPlacement/FailedPlacement", "[UnitPlacement]") {
     RTSPathingLib::RectangleGrid<bool> grid;
     grid.dimension = glm::ivec2(1, 1);
-    grid.data = { true };
+    grid.data = { false };
+    grid.offset = glm::dvec2(82, 82);
+    std::map<size_t, size_t> unitsToPlace;
+    size_t size = 1;
+    unitsToPlace[size] = 1;
+    size_t category = 123;
+    RTSPathingLib::UnitPlacement placement(grid, unitsToPlace, 123);
+
+    bool success = false;
+    std::vector<RTSPathingLib::Body> result = placement.place(success);
+    REQUIRE(!success);
+    REQUIRE(result.size() == 0);
+  }
+
+  TEST_CASE("UnitPlacement/OneSize2", "[UnitPlacement]") {
+    RTSPathingLib::RectangleGrid<bool> grid;
+    grid.dimension = glm::ivec2(2, 2);
+    grid.data = { true , true ,
+                  true , true };
     grid.offset = glm::dvec2(82, 82);
     std::map<size_t, size_t> unitsToPlace;
     size_t size = 2;
@@ -43,8 +61,10 @@ namespace BodyUtilTests {
 
   TEST_CASE("UnitPlacement/OneSize3", "[UnitPlacement]") {
     RTSPathingLib::RectangleGrid<bool> grid;
-    grid.dimension = glm::ivec2(1, 1);
-    grid.data = { true };
+    grid.dimension = glm::ivec2(3, 3);
+    grid.data = { true, true, true,
+                  true, true, true,
+                  true, true, true };
     grid.offset = glm::dvec2(82, 82);
     std::map<size_t, size_t> unitsToPlace;
     size_t size = 3;
@@ -57,5 +77,23 @@ namespace BodyUtilTests {
     REQUIRE(success);
     REQUIRE(result.size() == 1);
     REQUIRE(result[0].category == category);
+  }
+
+  TEST_CASE("UnitPlacement/FailedPlacementSize3", "[UnitPlacement]") {
+    RTSPathingLib::RectangleGrid<bool> grid;
+    grid.dimension = glm::ivec2(2, 2);
+    grid.data = { true, true,
+                  true, true};
+    grid.offset = glm::dvec2(82, 82);
+    std::map<size_t, size_t> unitsToPlace;
+    size_t size = 3;
+    unitsToPlace[size] = 1;
+    size_t category = 123;
+    RTSPathingLib::UnitPlacement placement(grid, unitsToPlace, 123);
+
+    bool success = false;
+    std::vector<RTSPathingLib::Body> result = placement.place(success);
+    REQUIRE(!success);
+    REQUIRE(result.size() == 0);
   }
 }
