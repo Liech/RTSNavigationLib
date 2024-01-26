c91ad99c257b764fbde45ecbefcce8cb375f1123
Liech <nicolas_harttig@hotmail.de>
2024-01-26 15:53:28 +0100

    improving flow field; dijkstra flow field impl

--- RTSNavigationLib/Source/RTSNavigationLib/Pathfinding/FlowField.cpp
+++ RTSNavigationLib/Source/RTSNavigationLib/Pathfinding/FlowField.cpp
@@ -9,22 +9,12 @@ namespace RTSNavigationLib {
   FlowField::FlowField(const DijkstraGrid& grid) {
     resolution = grid.getResolution();
     target = grid.getTarget();
-
-    int r = resolution.x;
-    std::vector<int> neighbours9 = { 
-      -r - 1,-r,-r+1,//0NNN0
-      -1,1,          //0NXN0
-      r-1,r,r+1      //0NNN0
-    };
-
     initField(grid);
   }
 
   FlowField::FlowField(const EikonalGrid& grid) {
     resolution = grid.getResolution();
     target = grid.getTarget();
-
-
     initField(grid);
   }
 
@@ -90,60 +80,76 @@ namespace RTSNavigationLib {
   }
 
   void FlowField::initField(const DijkstraGrid& grid) {
+    constexpr float maxVal = std::numeric_limits<float>::infinity();
     field.resize(resolution.x * resolution.y, 0);
 
+    constexpr const std::array<glm::ivec2,8> neighbourOffset = {
+      glm::ivec2(-1,-1),glm::ivec2(0,-1),glm::ivec2(1,-1),//NNN
+      glm::ivec2(-1,0),glm::ivec2(1,0),                   //NXN
+      glm::ivec2(-1,1),glm::ivec2(0,1),glm::ivec2(1,1),   //NNN
+    };
+    constexpr const std::array<size_t,4> elementsWithPreconditions = {
+      0,2,5,7
+    };
+    constexpr const std::array<size_t,8> converter8to16 = {
+      3,4,5,7,8,10,11,12
+    };
+
+#pragma omp parallel for
+    for (long long cell = 0; cell < field.size(); cell++) {
+      std::array<float, 8> neighbors;
+      long long x = cell % resolution.x;
+      long long y = cell / resolution.x;
+      bool anyWall = false;
+      for (size_t i = 0; i < 8; i++) {
+        const auto& offset = neighbourOffset[i];
+        auto pos = glm::ivec2(x, y) + offset;
+        bool isInXBound = pos.x >= 0 && pos.x < resolution.x;
+        bool isInYBound = pos.y >= 0 && pos.y < resolution.y;
+        size_t address = cell + offset.x + offset.y * resolution.x;
+        if (isInXBound && isInYBound)
+          neighbors[i] = grid.getDistance(address);
+        else
+          neighbors[i] = maxVal;
+        anyWall |= neighbors[i] == maxVal;
+      }
+
+      //preconditions
+      if (anyWall) {
+        for (const size_t& element : elementsWithPreconditions)
+          neighbors[element] = maxVal;
+      }
+
+      //select result
+      size_t bestID = 0;
+      float bestVal = neighbors[0];
+      for (size_t i = 0; i < 8; i++) {
+        const float& current = neighbors[i];
+        if (bestVal > current) {
+          bestID = i;
+          bestVal = current;
+        }
+      }
+      field[cell] = converter8to16[bestID]; //id can be converted via lookup table to vector
+    }
   }
 
   void FlowField::initField(const EikonalGrid& grid) {
     constexpr float maxVal = std::numeric_limits<float>::infinity();
     field.resize(resolution.x * resolution.y, 0);
 
-    int r = resolution.x;
-    const std::vector<glm::ivec2> neighbourOffset = {
+    constexpr const std::array<glm::ivec2,16> neighbourOffset = {
       glm::ivec2(-1,-2),glm::ivec2(1,-2),                                                    //0N0N0
       glm::ivec2(-2,-1),glm::ivec2(-1,-1),glm::ivec2(0,-1),glm::ivec2(1,-1),glm::ivec2(2,-1),//NNNNN
       glm::ivec2(-1,0),glm::ivec2(1,0),                                                      //0NXN0
       glm::ivec2(-2,1),glm::ivec2(-1,1),glm::ivec2(0,1),glm::ivec2(1,1),glm::ivec2(2,1),     //NNNNN
       glm::ivec2(-1,2),glm::ivec2(-1,2),                                                     //0N0N0
     };
-    const std::vector<std::vector<size_t>> mustFreeAreas{
-      //first one is a must, one of second or third must also
-      //A
-      {3,4,7}, //0N0N0    counting Ns
-               //NAaNN
-               //0aXN0
-               //NNNNN
-               //0N0N0
-      //B
-      {5,4,8},//0N0N0
-              //NNbBN
-              //0NXb0
-              //NNNNN
-              //0N0N0
-
-      //C
-      {10,7,11},//0N0N0
-                //NNNNN
-                //0cXN0
-                //NCcNN
-                //0N0N0
-
-      //D
-      {12,8,11},//0N0N0
-                //NNNNN
-                //0NXd0
-                //NNdDN
-                //0N0N0
+    constexpr const std::array<size_t,8> mustFreeAreas{
+      3,4,5,7,8,10,11,12
     };
-    const std::vector<size_t> preconditions = { //only counts if there is no wall in between
-      0,1,          //0A0B0
-      0,99,99,99,1, //ANNNB
-      99,99,        //0NXN0
-      2,99,99,99,3, //CNNND
-      2,3           //0C0D0
-    };
-    const std::vector<size_t> elementsWithPreconditions = {
-      0,1,2,6,9,13,14,15
+    constexpr const std::array<size_t,12> elementsWithPreconditions = {
+      0,1,2,3, 5,6,9,10 ,12,13,14,15
     };
 
 #pragma omp parallel for
@@ -164,13 +170,12 @@ namespace RTSNavigationLib {
       }
 
       //preconditions
-      for (const size_t& element : elementsWithPreconditions) {
-        size_t areaID = preconditions[element];
-        bool fullfilled = true;
-        for (const size_t& areaElement : mustFreeAreas[areaID]) {
-          fullfilled &= neighbors[areaElement] != maxVal;
-        }
-        if (!fullfilled)
+      bool freeCenter = true;
+      for (const size_t& element : mustFreeAreas) {
+          freeCenter &= neighbors[element] != maxVal;
+      }
+      if (!freeCenter) {
+        for (const size_t& element : elementsWithPreconditions)
           neighbors[element] = maxVal;
       }
 
--- Test/Tests/FlowFieldTest.cpp
+++ Test/Tests/FlowFieldTest.cpp
@@ -3,29 +3,83 @@
 
 #include "RTSNavigationLib/Pathfinding/FlowField.h"
 #include "RTSNavigationLib/Pathfinding/EikonalGrid.h"
+#include "RTSNavigationLib/Pathfinding/DijkstraGrid.h"
 
 namespace FlockTest {
   TEST_CASE("FlowField/EikonalSimple", "[EikonalSimpleSingleStep]") {
-    glm::ivec2 resolution = glm::ivec2(3, 3);
-    glm::ivec2 fromA = glm::ivec2(0, 1);
-    glm::ivec2 fromB = glm::ivec2(1, 0);
-    glm::ivec2 fromC = glm::ivec2(2, 1);
-    glm::ivec2 fromD = glm::ivec2(1, 2);
-    glm::ivec2 fromE = glm::ivec2(0, 0);
-    glm::ivec2 fromF = glm::ivec2(2, 0);
-    glm::ivec2 fromG = glm::ivec2(0, 2);
-    glm::ivec2 fromH = glm::ivec2(2, 2);
-    glm::ivec2 target= glm::ivec2(1, 1);
+    glm::ivec2 resolution = glm::ivec2(5, 5);
+    glm::ivec2 fromA = glm::ivec2(1+0, 1+1);
+    glm::ivec2 fromB = glm::ivec2(1+1, 1+0);
+    glm::ivec2 fromC = glm::ivec2(1+2, 1+1);
+    glm::ivec2 fromD = glm::ivec2(1+1, 1+2);
+    glm::ivec2 fromE = glm::ivec2(1+0, 1+0);
+    glm::ivec2 fromF = glm::ivec2(1+2, 1+0);
+    glm::ivec2 fromG = glm::ivec2(1+0, 1+2);
+    glm::ivec2 fromH = glm::ivec2(1+2, 1+2);
+    glm::ivec2 target= glm::ivec2(1+1, 1+1);
 
     float t = 1; //target
     float a = 1; float b = 1; float c = 1; float d = 1; float e = 1; float f = 1; float g = 1; float h = 1; // for visualization
     std::vector<float> obstacles = {
-      e,b,f,
-      a,t,c,
-      g,d,h 
+      1,1,1,1,1,
+      1,e,b,f,1,
+      1,a,t,c,1,
+      1,g,d,h,1,
+      1,1,1,1,1
     };
       
     RTSNavigationLib::EikonalGrid grid(obstacles, resolution, target);
+    RTSNavigationLib::FlowField field(grid);
+    glm::dvec2 directionA = field.getDirection(fromA);
+    glm::dvec2 directionB = field.getDirection(fromB);
+    glm::dvec2 directionC = field.getDirection(fromC);
+    glm::dvec2 directionD = field.getDirection(fromD);
+    glm::dvec2 directionE = field.getDirection(fromE);
+    glm::dvec2 directionF = field.getDirection(fromF);
+    glm::dvec2 directionG = field.getDirection(fromG);
+    glm::dvec2 directionH = field.getDirection(fromH);
+
+    REQUIRE(directionA[0] == glm::dvec2(1, 0)[0]);
+    REQUIRE(directionA[1] == glm::dvec2(1, 0)[1]);
+    REQUIRE(directionB[0] == glm::dvec2(0, 1)[0]);
+    REQUIRE(directionB[1] == glm::dvec2(0, 1)[1]);
+    REQUIRE(directionC[0] == glm::dvec2(-1, 0)[0]);
+    REQUIRE(directionC[1] == glm::dvec2(-1, 0)[1]);
+    REQUIRE(directionD[0] == glm::dvec2(0, -1)[0]);
+    REQUIRE(directionD[1] == glm::dvec2(0, -1)[1]);
+
+    REQUIRE(std::abs(directionE[0] - glm::normalize(glm::dvec2(1, 1))[0]) < 1e-6);
+    REQUIRE(std::abs(directionE[1] - glm::normalize(glm::dvec2(1, 1))[1]) < 1e-6);
+    REQUIRE(std::abs(directionF[0] - glm::normalize(glm::dvec2(-1, 1))[0]) < 1e-6);
+    REQUIRE(std::abs(directionF[1] - glm::normalize(glm::dvec2(-1, 1))[1]) < 1e-6);
+    REQUIRE(std::abs(directionG[0] - glm::normalize(glm::dvec2(1, -1))[0]) < 1e-6);
+    REQUIRE(std::abs(directionG[1] - glm::normalize(glm::dvec2(1, -1))[1]) < 1e-6);
+    REQUIRE(std::abs(directionH[0] - glm::normalize(glm::dvec2(-1, -1))[0]) < 1e-6);
+    REQUIRE(std::abs(directionH[1] - glm::normalize(glm::dvec2(-1, -1))[1]) < 1e-6);
+  }
+  TEST_CASE("FlowField/DijkstraSimple", "[DijkstraSimpleSingleStep]") {
+    glm::ivec2 resolution = glm::ivec2(5, 5);
+    glm::ivec2 fromA = glm::ivec2(1 + 0, 1 + 1);
+    glm::ivec2 fromB = glm::ivec2(1 + 1, 1 + 0);
+    glm::ivec2 fromC = glm::ivec2(1 + 2, 1 + 1);
+    glm::ivec2 fromD = glm::ivec2(1 + 1, 1 + 2);
+    glm::ivec2 fromE = glm::ivec2(1 + 0, 1 + 0);
+    glm::ivec2 fromF = glm::ivec2(1 + 2, 1 + 0);
+    glm::ivec2 fromG = glm::ivec2(1 + 0, 1 + 2);
+    glm::ivec2 fromH = glm::ivec2(1 + 2, 1 + 2);
+    glm::ivec2 target = glm::ivec2(1 + 1, 1 + 1);
+
+    float t = 1; //target
+    float a = 1; float b = 1; float c = 1; float d = 1; float e = 1; float f = 1; float g = 1; float h = 1; // for visualization
+    std::vector<float> obstacles = {
+      1,1,1,1,1,
+      1,e,b,f,1,
+      1,a,t,c,1,
+      1,g,d,h,1,
+      1,1,1,1,1
+    };
+
+    RTSNavigationLib::DijkstraGrid grid(obstacles, resolution, target);
     RTSNavigationLib::FlowField field(grid);
     glm::dvec2 directionA = field.getDirection(fromA);
     glm::dvec2 directionB = field.getDirection(fromB);
