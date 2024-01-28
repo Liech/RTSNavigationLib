09c9465b684228b8f724a323ff5429f7f3044696
Liech <nicolas_harttig@hotmail.de>
2024-01-28 23:40:54 +0100

    map: failing test

--- RTSNavigationLib/Source/RTSNavigationLib/Pathfinding/Map.cpp
+++ RTSNavigationLib/Source/RTSNavigationLib/Pathfinding/Map.cpp
@@ -3,11 +3,12 @@
 #include <stdexcept>
 
 namespace RTSNavigationLib {
-  Map::Map(const std::vector<float>& obstacles_, const glm::ivec2 resolution_, const glm::ivec2& subResolution_, bool eikonal_) {
+  Map::Map(const std::vector<float>& obstacles_, const glm::ivec2 resolution_, const glm::ivec2& subResolution_, size_t overlap_, bool eikonal_) {
     resolution    = resolution_;
     subResolution = subResolution_;
     obstacles     = obstacles_;
     eikonal       = eikonal_;
+    overlap       = overlap_;
   }
 
   size_t Map::getMap(const glm::ivec2& from, const glm::ivec2& to) {
@@ -26,11 +27,31 @@ namespace RTSNavigationLib {
     throw std::runtime_error("Not implemented Yet!");
   }
 
+  glm::ivec2 Map::getOffset(size_t id) const {
+    throw std::runtime_error("Not implemented Yet!");
+  }
+
   size_t Map::addCostStamp(const std::vector<float>& obstacles, const glm::ivec2 resolution) {
     throw std::runtime_error("Not implemented Yet!");
   }
 
   void Map::removeCostStamp(size_t) {
     throw std::runtime_error("Not implemented Yet!");
+  }
+
+  glm::ivec2 Map::getResolution() const {
+    return resolution;
+  }
+
+  glm::ivec2 Map::getSubResolution() const {
+    return subResolution;
+  }
+
+  bool Map::getEikonal() const {
+    return eikonal;
+  }
+
+  size_t Map::getOverlap() const {
+    return overlap;
   }
 }
--- RTSNavigationLib/Source/RTSNavigationLib/Pathfinding/Map.h
+++ RTSNavigationLib/Source/RTSNavigationLib/Pathfinding/Map.h
@@ -8,21 +8,28 @@ namespace RTSNavigationLib {
 
   class Map {
   public:
-    Map(const std::vector<float>& obstacles,const glm::ivec2 resolution, const glm::ivec2& subResolution, bool eikonal = true);
+    Map(const std::vector<float>& obstacles,const glm::ivec2 resolution, const glm::ivec2& subResolution, size_t overlap, bool eikonal = true);
 
           size_t     getMap(const glm::ivec2& from, const glm::ivec2& to);    
           float      getDistance(const glm::ivec2& from, const glm::ivec2& to);
           bool       hasMap(size_t id) const;
     const FlowField& getMap(size_t id) const;
+    glm::ivec2 getOffset(size_t id) const;
 
     size_t addCostStamp(const std::vector<float>& obstacles, const glm::ivec2 resolution);
     void   removeCostStamp(size_t);
+
+    glm::ivec2 getResolution()    const;
+    glm::ivec2 getSubResolution() const;
+    bool       getEikonal()       const;
+    size_t     getOverlap()       const;
 
   private:
     glm::ivec2         resolution;
     glm::ivec2         subResolution;
     std::vector<float> obstacles;
     bool               eikonal;
+    size_t             overlap;
     
   };
 }
