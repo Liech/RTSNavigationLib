a14779452a9874f58d3521dd73f39661fdff4b71
Liech <nicolas_harttig@hotmail.de>
2024-11-26 22:05:37 +0100

    try fix submodules

--- godot/Scripts/compileBindings.bat
+++ godot/Scripts/compileBindings.bat
@@ -5,11 +5,11 @@ call "..\\emsdk\\emsdk_env.bat" activate
 
 cd ..
 cd out
-call .\\engine\\godot.windows.editor.x86_64.exe --dump-extension-api
+call .\\godot\\godot.windows.editor.x86_64.exe --dump-extension-api
 cd ..
 cd godot-cpp
-REM call ..\\scons\\scons platform=windows bits=64 custom_api_file=..\\out\\extension_api.json
-call ..\\scons\\scons platform=web dlink_enabled=yes custom_api_file=..\\out\\extension_api.json
+call ..\\scons\\scons platform=windows bits=64 custom_api_file=..\\out\\extension_api.json
+rem call ..\\scons\\scons platform=web dlink_enabled=yes custom_api_file=..\\out\\extension_apiweb.json
 
 xcopy /s ..\\godot-cpp\\bin ..\\out\\godot-cpp\\* /Y
 
