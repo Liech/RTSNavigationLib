ada51c600b8bc47e0dc623f8bdbd977bd52a8132
Liech <nicolas_harttig@hotmail.de>
2024-11-30 16:30:37 +0100

    2d rts cam finished

--- godot/GodotProject/RTS2DCam.gd
+++ godot/GodotProject/RTS2DCam.gd
@@ -1,17 +1,26 @@
 extends Camera2D
 
-
-const MOVE_SPEED = 400
+@export var move_speed : float = 800
+@export var zoom_speed : float = 0.1
 
 func _process(delta : float) -> void:
 	var x : float = Input.get_axis("ui_left", "ui_right")
 	var y : float = Input.get_axis("ui_down", "ui_up")
-	global_position += Vector2.RIGHT * delta * MOVE_SPEED * x + Vector2.UP * delta * MOVE_SPEED * y
+	global_position += (Vector2.RIGHT * x + Vector2.UP * y) * delta * move_speed * (1.0/zoom.length())
 	
 	if (Input.is_action_just_released("zoom_in")):
-		zoom_on(Vector2(0,0),1.05)
+		zoom_on(get_global_mouse_position(),1+zoom_speed)
 	if (Input.is_action_just_released("zoom_out")):
-		zoom_on(Vector2(0,0),0.95)
+		zoom_on(get_global_mouse_position(),1-zoom_speed)
+	print(get_global_view_rect())
+
+func get_global_view_rect() -> Rect2:
+	var t := get_canvas_transform()
+	var camera_rect := t.affine_inverse() * get_viewport_rect()
+	return camera_rect
 
 func zoom_on(location : Vector2, factor : float) ->void:
+	var rect:=get_global_view_rect()
+	offset += (rect.get_center()-location) * (1-factor)
 	zoom *= factor
+	
