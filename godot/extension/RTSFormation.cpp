f8605656b648c46685c0eb7641cfdd5c5f3f9f0a
Liech <nicolas_harttig@hotmail.de>
2024-12-01 19:50:47 +0100

    added some properties

--- godot/extension/RTSFormation.cpp
+++ godot/extension/RTSFormation.cpp
@@ -3,19 +3,36 @@
 
 namespace godot {
   void RTSFormation::_bind_methods() {
+    BIND_ENUM_CONSTANT(center_first_rts);
+    BIND_ENUM_CONSTANT(outer_first_rts);
+    BIND_ENUM_CONSTANT(distribute_evenly_rts);
+
     ClassDB::bind_method(D_METHOD("get_category"), &RTSFormation::get_category);
     ClassDB::bind_method(D_METHOD("set_category", "category"), &RTSFormation::set_category);
     ClassDB::bind_method(D_METHOD("get_children"), &RTSFormation::get_children);
     ClassDB::bind_method(D_METHOD("set_children", "children"), &RTSFormation::set_children);
+    ClassDB::bind_method(D_METHOD("get_placement_behavior"), &RTSFormation::get_placement_behavior);
+    ClassDB::bind_method(D_METHOD("set_placement_behavior", "placement_behavior"), &RTSFormation::set_placement_behavior);
+    ClassDB::bind_method(D_METHOD("get_unit_distribution_weight"), &RTSFormation::get_unit_distribution_weight);
+    ClassDB::bind_method(D_METHOD("set_unit_distribution_weight", "placement_behavior"), &RTSFormation::set_unit_distribution_weight);
+    ClassDB::bind_method(D_METHOD("get_rotation"), &RTSFormation::get_rotation);
+    ClassDB::bind_method(D_METHOD("set_rotation", "rotation"), &RTSFormation::set_rotation);
 
     ADD_PROPERTY(PropertyInfo(Variant::INT, "category"), "set_category", "get_category");
+    ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "weight"), "set_unit_distribution_weight", "get_unit_distribution_weight");
+    ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "rotation"), "set_rotation", "get_rotation");
+    ClassDB::add_property(get_class_static(), PropertyInfo(Variant::INT, "placement_behavior", PROPERTY_HINT_ENUM, "center_first,outer_first_rts,distribute_evenly_rts", PROPERTY_USAGE_DEFAULT), "set_placement_behavior", "get_placement_behavior");
+
     ClassDB::add_property("RTSFormation", PropertyInfo(
-      Variant::ARRAY,
+      Variant::ARRAY, 
       "children",
       PROPERTY_HINT_TYPE_STRING,
       String::num(Variant::OBJECT) + "/" + String::num(PROPERTY_HINT_RESOURCE_TYPE) + ":RTSFormation"
     ), "set_children", "get_children");
 
+
+
+  
   }
 
   RTSFormation::RTSFormation() {
@@ -30,4 +47,10 @@ namespace godot {
   int RTSFormation::get_category() const { return category; }
   void RTSFormation::set_children(const TypedArray<RTSFormation>& children_) { children = children_; }
   TypedArray<RTSFormation> RTSFormation::get_children() const { return children; }
+  void RTSFormation::set_placement_behavior(const RTSFormation::PlacementBehavior b) { placementBehavior = b; }
+  RTSFormation::PlacementBehavior RTSFormation::get_placement_behavior() const { return placementBehavior; }
+  void RTSFormation::set_unit_distribution_weight(const float w) { unitDistributionWeight = w; }
+  float RTSFormation::get_unit_distribution_weight() const { return unitDistributionWeight; }
+  void RTSFormation::set_rotation(const float rot) { rotation = rot; }
+  float RTSFormation::get_rotation() const { return rotation; }
 }
--- godot/extension/RTSFormation.h
+++ godot/extension/RTSFormation.h
@@ -6,20 +6,38 @@
 namespace godot {
   class RTSFormation : public Resource {
     GDCLASS(RTSFormation, Resource)
-
-  protected:
-    static void _bind_methods();
-
   public:
     RTSFormation();
     ~RTSFormation();
 
+
+    enum PlacementBehavior {
+      center_first_rts, outer_first_rts, distribute_evenly_rts
+    };
+
+  protected:
+    static void _bind_methods();
+
+  private:
+    void set_placement_behavior(const PlacementBehavior cat);
+    PlacementBehavior get_placement_behavior() const;
     void set_category(const int cat);
     int get_category() const;
+    void set_unit_distribution_weight(const float cat);
+    float get_unit_distribution_weight() const;
+    void set_rotation(const float cat);
+    float get_rotation() const;
     void set_children(const TypedArray<RTSFormation>& cat);
     TypedArray<RTSFormation> get_children() const;
-  private:
+
+
+    PlacementBehavior placementBehavior = PlacementBehavior::center_first_rts;
     int category = 0;
+    float unitDistributionWeight = 1.0;
+    float rotation = 0.0;
+
     TypedArray<RTSFormation> children;
   };
 }
+
+VARIANT_ENUM_CAST(godot::RTSFormation::PlacementBehavior);
