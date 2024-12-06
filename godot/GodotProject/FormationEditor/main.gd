8ed3618e60f059a839ab2388a45205c65858c3e7
Liech <nicolas_harttig@hotmail.de>
2024-12-07 00:07:17 +0100

    started formation ui

--- godot/GodotProject/FormationEditor/main.gd
+++ godot/GodotProject/FormationEditor/main.gd
@@ -3,8 +3,14 @@ extends Node
 @export var start_unit_types : Array[UnitType]
 @export var start_categories : Array[String]
 
+@onready var unit_scene : Node = $Scenes/UnitScene
+
 func _ready() -> void:
 	FormationEditor.categories = start_categories
 	FormationEditor.unitTypes = start_unit_types
 	FormationEditor.unit_types_changed.emit()
 	FormationEditor.categories_changed.emit()
+
+
+func _on_characters_pressed() -> void:
+	unit_scene.visible = true
