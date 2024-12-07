ee2720b1aa02e81ecd9b54efb66ff98947a4ebc3
Liech <nicolas_harttig@hotmail.de>
2024-12-07 01:07:31 +0100

    added formation properties

--- godot/GodotProject/FormationEditor/Formation/formation.gd
+++ godot/GodotProject/FormationEditor/Formation/formation.gd
@@ -2,3 +2,12 @@ class_name Formation extends Resource
 
 @export var children : Array[Formation]
 @export var name : String
+
+@export var own_InterfacePoint : int 
+@export var parent_interface_point : int     
+@export var overwrite_width_with_interface_width : bool
+@export var rotate_with_interface  : bool
+@export var rotation : float
+@export var category : String
+@export var weight : float
+@export var placement_behavior : RTSFormation.PlacementBehavior              
